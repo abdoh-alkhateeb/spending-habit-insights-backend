19e7222e0562051fef9f369de0f9fb61fc0394e2
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2024-11-15 22:05:40 +0200

    x

--- src/server.ts
+++ src/server.ts
@@ -1,28 +1,16 @@
 import express, { Request, Response } from 'express';
-// import multer from 'multer';
 
 const app = express();
-const port = 5000;
+const port = parseInt(process.env.PORT || '5000');
 
-// const fileBuffers: Buffer[] = [];
-
-// const storage = multer.memoryStorage();
-// const upload = multer({ storage: storage });
-
-// app.post('/api/upload', upload.single('file'), (req: Request, res: Response) => {
-//   if (!req.file) {
-//     return res.status(400).send('No file uploaded.');
-//   }
-
-//   fileBuffers.push(req.file.buffer);
-
-//   res.status(200).send('File uploaded successfully.');
-// });
+app.get('/', (req: Request, res: Response) => {
+  res.send('Server is running');
+});
 
 app.post('/api/upload', (req: Request, res: Response) => {
   res.send('hello');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+app.listen(port, '0.0.0.0', () => {
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
